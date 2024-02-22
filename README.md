91144d1694aa1a1feb67579a7799a7b51a1f54d3
dcamachop <camachopereira18@hotmail.es>
2024-02-22 17:08:48 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# practica01
+ Practica 01 Curso Lunes
---
+++ src/main/java/com/practica01/Practica01CursoLunesApplication.java
@@ -0,0 +1,13 @@
+package com.practica01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica01CursoLunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica01CursoLunesApplication.class, args);
+	}
+
+}
